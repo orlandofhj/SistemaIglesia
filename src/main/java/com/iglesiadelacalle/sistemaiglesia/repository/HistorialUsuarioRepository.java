ab162ff00e1bd5da62e40f657036a5203a9638a6
orlandofhj <orlandofhj@gmail.com>
2026-09-17 22:11:19 -0400

    Delete src/main/java/com/iglesiadelacalle/sistemaiglesia directory

--- src/main/java/com/iglesiadelacalle/sistemaiglesia/repository/HistorialUsuarioRepository.java
+++
@@ -1,9 +0,0 @@
-package com.iglesiadelacalle.sistemaiglesia.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-import com.iglesiadelacalle.sistemaiglesia.models.HistorialUsuario;
-
-@Repository
-public interface HistorialUsuarioRepository extends JpaRepository<HistorialUsuario, Integer> {
-}
--- src/main/java/com/iglesiadelacalle/sistemaiglesia/repository/RolRepository.java
+++
@@ -1,9 +0,0 @@
-package com.iglesiadelacalle.sistemaiglesia.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-import com.iglesiadelacalle.sistemaiglesia.models.Rol;
-
-@Repository
-public interface RolRepository extends JpaRepository<Rol, Integer> {
-}
